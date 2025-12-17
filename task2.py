aefef762e7093edcec7731223c1a92320c0a877f
evalewis055-ai <eva.lewis055@students.cmcss.net>
2025-12-17 08:26:02 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,3 +1,4 @@
+#Do Not use Ctrl+A to select code. Only copy the code below this line.
 #------------------------------------------------------------------------------------------------
 # Week 1: Chores and first purchase
 allowance += 4
