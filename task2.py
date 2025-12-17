73cc373b5fb2fb55f4fcecd0c11552d3120dc10a
evalewis055-ai <eva.lewis055@students.cmcss.net>
2025-12-17 08:53:40 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -6,12 +6,12 @@ allowance += lawn
 allowance -= candy
 
 # Week 2: Bonus week and purchase
-allowance *= 3
+allowance *= allowance
 allowance += vacuum
 allowance -= toy
 
 # Week 3: Savings
-allowance /= 2
+allowance /= allowance
 
 # Print final allowance
 print(f"Allowance: $43.0")
