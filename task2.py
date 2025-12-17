475c75200a9b211cd821c0c90526b85b92e96fc8
evalewis055-ai <eva.lewis055@students.cmcss.net>
2025-12-17 08:49:39 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,12 +1,14 @@
+
+#------------------------------------------------------------------------------------------------
 # Week 1: Chores and first purchase
-allowance += 4
-allowance += 10
-allowance -= 5
+allowance += dishes
+allowance += lawn
+allowance -= candy
 
 # Week 2: Bonus week and purchase
 allowance *= 3
-allowance += 7
-allowance -= 8
+allowance += vacuum
+allowance -= toy
 
 # Week 3: Savings
 allowance /= 2
